fbe2447bc778cf6f62c9787fb98f3de19757dd69
asimashfaq <asimizb4@gmail.com>
2019-09-13 13:38:25 +0500

    improvement

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,10 @@ interface ILetterCounter {
 export const letterCounter = (input:string):ILetterCounter[] => {
   let letterCountArray:ILetterCounter[]  = new Array<ILetterCounter>();
   for (let i = 0; i < input.length; i++) {
-    
     if (letterCountArray[input[i]] == undefined){
-      letterCountArray[input[i]] = {letter:input[i],occurences:1}
-
-    }else{
-      letterCountArray[input[i]].occurences++; 
+      letterCountArray[input[i]] = {letter:input[i],occurences:0}
     }
-    
+    letterCountArray[input[i]].occurences++; 
   }
-
   return letterCountArray;
 };
